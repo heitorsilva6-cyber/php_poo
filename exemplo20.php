45710e4dc2814776c0ecc1c35968da36bfef8253
heitorsilva6-cyber <heitor.m.silva6@aluno.senai.br>
2026-04-29 16:40:19 -0300

    exemplos

--- exemplo20.php
+++ exemplo20.php
@@ -22,3 +22,23 @@ class NotificadorWhatsapp implements INotificador {
         echo "Enviando WhatsApp para $destinatario: $mensagem <br>";
     }
 }
+
+class SistemaNotificacoes {
+    private $notificador;
+
+    public function __construct(INotificador $notificador) {
+        $this->notificador = $notificador;
+    }
+
+    public function enviarNotificacao($destinatario, $mensagem) {
+        $this->notificador->enviar($destinatario, $mensagem);
+    }
+}
+
+$sistemasEmail = new SistemaNotificacoes(new EmailNotificador());
+$sistemasSMS = new SistemaNotificacoes(new SMSNotificador());
+$sistemasWhatsapp = new SistemaNotificacoes(new NotificadorWhatsapp());
+
+$sistemasEmail->enviarNotificacao("joao@gmail.com", "Seu pedido foi confirmado!");
+$sistemasSMS->enviarNotificacao("11999999999", "Seu pedido foi confirmado!");
+$sistemasWhatsapp->enviarNotificacao("11999999999", "Seu pedido foi confirmado!");
